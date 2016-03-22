b7d6667cc4a4d091cd6fce08759fb8cdc620d8d3
Arslan Memon <muhammadmemon@Muhammads-MBP.fios-router.home>
2016-03-22 01:08:21 -0500

    updated db design

--- backend/db_design/alec.sql
+++ backend/db_design/alec.sql
@@ -5,13 +5,19 @@ use ALECTutor;
 CREATE TABLE Tutors(
   tutor_id INT primary key,
   first_name VARCHAR(45),
-  last_name VARCHAR(45)
+  last_name VARCHAR(45),
+  email VARCHAR(45),
+  phone VARCHAR(45)
 );
 CREATE TABLE Sessions(
   session_id INT primary key,
   session_date DATE,
   session_time VARCHAR(45),
-  tutor_id INT
+  tutor_id INT,
+  student_id VARCHAR(45),
+  student_email VARCHAR(45),
+  session_start_time VARCHAR(45),
+  session_end_time VARCHAR(45)
 );
 CREATE TABLE Timeslots(
   timeslot_id INT primary key,
@@ -20,7 +26,7 @@ CREATE TABLE Timeslots(
   tutor_id INT
 );
 CREATE TABLE Reviews(
-  review_id INT primary kjey,
+  review_id INT primary key,
   score_1 INT,
   score_2 INT,
   score_3 INT,
@@ -34,8 +40,17 @@ CREATE TABLE ApplicantTimeslots(
 );
 CREATE TABLE Applications(
   application_id INT primary key,
-  first_name VARCHAR(45),
-  last_name VARCHAR(45),
-  applicant_id INT,
+  answer_1 VARCHAR(45),
+  answer_2 VARCHAR(45),
+  answer_3 VARCHAR(45),
   satus VARCHAR(45)
 );
+CREATE TABLE Applicants(
+  applicant_id INT primary key,
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  email VARCHAR(45),
+  gpa decimal(3).
+  application_id INT
+
+)
