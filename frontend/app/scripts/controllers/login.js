e2971dd27a90cee0fb0367928fc9f590503286f4
Jayce Miller <jaycem@smu.edu>
2016-03-29 16:23:51 -0500

    wrote most of the angular, just need to figure out why not talking to controller

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -8,14 +8,35 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, $http) {
 
     console.log('inside controller');
-    $scope.master = {};
-    $scope.email = '';
 
-    $scope.update = function(user) {
-      $scope.master = angular.copy(user);
-    };
+
+    $scope.signIn = function(user) {
+      var user_data = {
+        'userID': user.userID,
+        'password': user.password
+      };
+
+      var user_json = JSON.stringify(user_data);
+
+      $http({
+        method: 'POST',
+        url: 'http://54.86.70.62/login',
+        data: user_json
+        }).then(function successCallback(response) {
+          if(response.data == "Success") {
+            $scope.user = '';
+            alert("Log In Successful");
+
+          }
+          }, function errorCallback(response) {
+            console.log(response);
+            alert("Log In Unsuccessful");
+            $scope.user = '';
+
+          });
+        };
 
   });
