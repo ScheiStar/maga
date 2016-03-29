af8d09da1816c46980dc7cf2e736a0ce8c55f327
Jayce Miller <jaycem@smu.edu>
2016-03-29 15:36:51 -0500

    starting userDash

--- frontend/app/scripts/controllers/userdash.js
+++ frontend/app/scripts/controllers/userdash.js
@@ -9,9 +9,14 @@
  */
 angular.module('frontendApp')
   .controller('UserdashCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    // this.awesomeThings = [
+    //   'HTML5 Boilerplate',
+    //   'AngularJS',
+    //   'Karma'
+    // ];
+
+    $scope.example = {
+        text: 'guest',
+        word: /^\s*\w*\s*$/
+      };
   });
