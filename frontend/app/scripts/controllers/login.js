202b5a74647f2b391db52124b51bb848cb902c7a
Matthew Lee <matthew.lee711@gmail.com>
2016-03-28 21:12:12 -0500

    Most recent login page

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -7,11 +7,14 @@
  * # LoginCtrl
  * Controller of the frontendApp
  */
-angular.module('frontendApp')
-  .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+var login = angular.module('frontendApp',[]);
+
+
+login.controller('LoginCtrl', [$scope, function($scope) {
+    // this.awesomeThings = [
+    //   'HTML5 Boilerplate',
+    //   'AngularJS',
+    //   'Karma'
+    // ];
+    $scope.inputEmail = 0;
+  }]);
