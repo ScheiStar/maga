ed9e4f13dec2ba418c0c24f5ad0f68eaddc5b249
Andrew Ksendzoff <dacoconuttman@gmail.com>
2016-04-01 13:40:49 -0500

    Added /getTutors endpoints

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -124,3 +124,51 @@ $app->get('/getApplications', function($request, $response, $args) {
 	
 });
 
+$app->get('/admin/getTutors',function($request,$response,$args){
+  //$query = mysql_query("SELECT * FROM Tutor");
+      $db = $this->alec;
+         $result=$db->query('select * from Tutor');
+         $temp = array();
+         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+           $temp[] = $row;
+         }
+echo json_encode($temp);
+// In case any of o
+});
+$app->get('/admin/getTutors/{id}',function($request,$response,$args){
+  //$query = mysql_query("SELECT * FROM Tutor");
+        $id=$args['id'];
+        if(!isset($id))
+        {
+          $new_response=$response->withStatus(400);
+          echo "please provide a user id";
+          return $new_response;
+        }
+        $db = $this->alec;
+         $result=$db->query('select * from Tutor where id='.$id);
+         $user=$result->fetch(PDO::FETCH_OBJ);
+         if ($user){
+           echo json_encode($user);
+           return $response;
+         }
+         else{
+           $new_response=$response->withStatus(204);
+           echo "user not found";
+           return $new_response;
+         }
+});
+$app->delete('/admin/getTutors/{id}',function($request,$response,$args){
+  //$query = mysql_query("SELECT * FROM Tutor");
+        $id=$args['id'];
+        if(!isset($id))
+        {
+          $new_response=$response->withStatus(400);
+          echo "please provide a user id";
+          return $new_response;
+        }
+        $db = $this->alec;
+        $result=$db->query('delete from Tutor where id='.$id);
+        echo 'success';
+   return $response;
+});
+
