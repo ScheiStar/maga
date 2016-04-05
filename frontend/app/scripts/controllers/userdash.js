25353d822c2753e6637ffd0f2868cc8ef4f60323
Jayce Miller <jaycem@smu.edu>
2016-04-05 18:52:07 -0500

    userdash changes

--- frontend/app/scripts/controllers/userdash.js
+++ frontend/app/scripts/controllers/userdash.js
@@ -12,24 +12,27 @@
   // 	return{classes: {}};
   // })
 angular.module('frontendApp')
-<<<<<<< HEAD
   .controller('UserdashCtrl', function ($state, $scope, $http, userFactory, $window) {
-    
-  	$scope.classArr = { classInfo: ['CSE1342','CSE2340'] };
-  	
-    console.log("in userDash now");
-    console.log(userFactory.getCurrentUser());
-    $scope.user = userFactory.getCurrentUser();
 
-  	$scope.getHelp = function() {
-  		$state.go("helpdash");
-  		console.log('helpdash');
-  	}
+  	//$scope.classArr = { classInfo: ['CSE1342','CSE2340'] };
+
+    console.log("in userDash now");
+    // console.log(userFactory.getCurrentUser());
+    // $scope.user = userFactory.getCurrentUser();
+
+    userFactory.saveToken()
+    var userID = (userFactory.parseToken(userFactory.getToken()));
+    console.log(userID);
+
+  	// $scope.getHelp = function() {
+  	// 	$state.go("helpdash");
+  	// 	console.log('helpdash');
+  	// }
 
 
   //   $scope.classesData = function(){
-  	$http.get('http://54.86.70.62/admin/getTutor/?format=json').success(function(data) {
-        $scope.classes = data;
+  	//$http.get('http://54.86.70.62/admin/getTutor/?format=json').success(function(data) {
+        //$scope.classes = data;
         // for (var i=0; i < $scope.classes.results.length; i++)
         // {
         //     if ($scope.classes.results.status == 0)
@@ -37,7 +40,7 @@ angular.module('frontendApp')
         //         tobedone++;
         //     }
         // }
-    });
+  //  });
 
   //   	$http({
 		//   method: 'GET',
