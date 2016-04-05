a95a984dcb28d3d575e894cf4e808bd00344a962
Matthew Lee <matthew.lee711@gmail.com>
2016-04-05 18:43:34 -0500

    fixing js

--- frontend/app/scripts/controllers/userdash.js
+++ frontend/app/scripts/controllers/userdash.js
@@ -7,16 +7,50 @@
  * # UserdashCtrl
  * Controller of the frontendApp
  */
-angular.module('frontendApp')
-  .controller('UserdashCtrl', function () {
-    // this.awesomeThings = [
-    //   'HTML5 Boilerplate',
-    //   'AngularJS',
-    //   'Karma'
-    // ];
 
-    $scope.example = {
-        text: 'guest',
-        word: /^\s*\w*\s*$/
-      };
+// app.factory('classesData', function(){
+  // 	return{classes: {}};
+  // })
+angular.module('frontendApp')
+<<<<<<< HEAD
+  .controller('UserdashCtrl', function ($state, $scope, $http, userFactory, $window) {
+    
+  	$scope.classArr = { classInfo: ['CSE1342','CSE2340'] };
+  	
+    console.log("in userDash now");
+    console.log(userFactory.getCurrentUser());
+    $scope.user = userFactory.getCurrentUser();
+
+  	$scope.getHelp = function() {
+  		$state.go("helpdash");
+  		console.log('helpdash');
+  	}
+
+
+  //   $scope.classesData = function(){
+  	$http.get('http://54.86.70.62/admin/getTutor/?format=json').success(function(data) {
+        $scope.classes = data;
+        // for (var i=0; i < $scope.classes.results.length; i++)
+        // {
+        //     if ($scope.classes.results.status == 0)
+        //     {
+        //         tobedone++;
+        //     }
+        // }
+    });
+
+  //   	$http({
+		//   method: 'GET',
+		//   url: 'http://54.86.70.62/admin/getTutor'
+		//   params: {abv: userFactory.getCurrentUser().abv}
+		// }).then(function successCallback(response) {
+		//     // this callback will be called asynchronously
+		//     // when the response is available
+		//     //classesData.classes = response.data.classes;
+		//     console.log('success');
+		//   }, function errorCallback(response) {
+		//     console.log('fail');
+		//   });
+
+    //});
   });
