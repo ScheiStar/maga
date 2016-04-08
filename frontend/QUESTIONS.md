8756b530c4d41aa3804fe7bd11c9d56bb84fbfe1
CT-Kyle <ckyle@smu.edu>
2016-04-08 18:01:29 -0500

    AppForm is integrated into code base now, some stylistic errors and loading first form error

---
+++ frontend/QUESTIONS.md
@@ -0,0 +1,8 @@
+# Ongoing questions that need to be answered
+## Application Form
+- How do I validate/require the fields when using the UI-router with nested states? The simple "require" doesn't work (Caleb).
+- How do I make "confirm password" actually confirm password? (Caleb)
+- How can I validate course numbers and letter grades in AppForm2? (Caleb)
+- Calendar looks alright, how can I make just the cell clickable and not the actual checkbox? (Caleb)
+- How can we do the calendar entry JSON better? (Caleb)
+- How can we display calendar better from the JSON in AppForm3, other than just linking them back to the original? (Caleb)
--- frontend/app/scripts/controllers/applicationform.js
+++ frontend/app/scripts/controllers/applicationform.js
@@ -8,10 +8,24 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('ApplicationformCtrl', function () {
-    this.awesomeThings = [
+
+.controller('formController', function($scope) {
+    console.log("In formController");
+      this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ];
-  });
+    ];  
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    // function to process the form
+    $scope.processForm = function() {
+        alert('Thanks for Applying!');  
+    };
+//    $scope.apply = function() {
+//        console.log("apply")
+//      $state.go("main.html");
+//    };
+    
+});
+
