6cd6746d28c11e2b2bcffb2d7f3bea5f5c1fdd73
Matthew Lee <matthew.lee711@gmail.com>
2016-04-09 15:04:35 -0500

    Merge frontend w/userDash -- Going to implement get requests

--- frontend/app/scripts/controllers/userdash.js
+++ frontend/app/scripts/controllers/userdash.js
@@ -16,6 +16,7 @@ angular.module('frontendApp')
 
   	$scope.classArr = { classInfo: ['CSE1342','CSE2340'] };
     $scope.add = {};
+    $scope.getUserTest = {};
   	// //Store class input -- json
     var classData = {
       'userID': userFactory.getToken(),
@@ -40,17 +41,31 @@ angular.module('frontendApp')
       // });
     }
     
-    // $http({
-    //   method: 'GET',
-    //   url: 'http://54.86.70.62/admin/getTutor'
-    //   params: {'userID': userID}
-    // }).then(function successCallback(response) {
-    //     // this callback will be called asynchronously
-    //     // when the response is available
-    //     //classesData.classes = response.data.classes;
-    //     console.log('success');
-    //   }, function errorCallback(response) {
-    //     console.log('fail');
-    //   });
+    $scope.getUser = function() {
+      console.log("uuu");
+      $http({
+      method: 'GET',
+      url: 'http://54.86.70.62/admin/getTutors'
+      //params: {'userID': userID}
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        //classesData.classes = response.data.classes;
+        $scope.getUserTest = response;
+        console.log('success');
+        console.log($scope.getUserTest);
+      }, function errorCallback(response) {
+        console.log('fail');
+      });
+
+    //if(userID = $scope.getUserTest.data.)
+    // for(var i = 0; i < $scope.getUserTest.data; i++) {
+    //   if(userID = $scope.getUserTest.data.tutorID) {
+
+    //   }
+    // }
+
+    }
+    
 
   });
