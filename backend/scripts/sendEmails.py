e28d767a02841a6541625a80666a572c2e3d0491
Andrew Ksendzoff <dacoconuttman@gmail.com>
2016-04-10 22:33:41 -0500

    Update sendEmails.py

--- backend/scripts/sendEmails.py
+++ backend/scripts/sendEmails.py
@@ -39,27 +39,27 @@ if emailType not in emailTypeList:
 if(email and emailType):
 
     if emailType == 'appConfirm':
-        f = open("/var/www/maga/scripts/html/html/confirmApp.html","r")
+        f = open("/var/www/maga/scripts/html/confirmApp.html","r")
         message = f.read()
 
     elif emailType == 'contactAdminConfirm':
-        f = open("/var/www/maga/scripts/html/html/confirmAdminFirst.html","r")
+        f = open("/var/www/maga/scripts/html/confirmAdminFirst.html","r")
         message = f.read() + str(contentMessage) + '</p></body></html>'
 
     elif emailType == 'emailToAdmin':
-        f = open("/var/www/maga/scripts/html/html/emailAdmin.html","r")
+        f = open("/var/www/maga/scripts/html/emailAdmin.html","r")
         message = f.read() + str(contentMessage) + '</p><br>' + str(email) + '</body></html>'
         #input whoever admin email in. For this purpose we only have one admin
         email = 'jaycem@smu.edu'
 
     elif emailType == 'tutorQuit':
-        f = open("/var/www/maga/scripts/html/html/tutorQuit.html","r")
+        f = open("/var/www/maga/scripts/html/tutorQuit.html","r")
         message = f.read() + str(contentMessage) + '</p><br>' + str(email) + '</body></html>'
         #input whoever admin email in. For this purpose we only have one admin
         email = 'jaycem@smu.edu'
 
     elif emailType == 'appAcceptance':
-        f = open("/var/www/maga/scripts/html/html/appAcceptance.html","r")
+        f = open("/var/www/maga/scripts/html/appAcceptance.html","r")
         message = f.read()
 
     elif emailType == 'passRecovery':
