88ab21ffa29904e4c881de8eeb53f5e6020f5ab9
Jayce Miller <jaycem@smu.edu>
2016-04-21 19:11:03 -0500

    idk

--- frontend/app/scripts/controllers/testtablw.js
+++ frontend/app/scripts/controllers/testtablw.js
@@ -9,6 +9,19 @@
  */
 angular.module('frontendApp')
   .controller('TesttablwCtrl', function ($scope) {
+
+  //   var data = [
+  //   {
+  //     z: [[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+  //     x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  //     y: ['Morning', 'Afternoon', 'Evening'],
+  //     type: 'heatmap'
+  //   }
+  // ];
+  //
+  //   Plotly.newPlot('myDiv', data);
+
+
     $scope.ugh = new Array();
     $scope.real = new Array();
     var shitData = {"data":
