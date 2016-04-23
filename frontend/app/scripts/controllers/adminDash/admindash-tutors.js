c1e3d7fe8e8b0572b54751b0cf6ae5c4a130caeb
Jayce Miller <jaycem@smu.edu>
2016-04-23 16:16:04 -0500

    view tutor now

--- frontend/app/scripts/controllers/adminDash/admindash-tutors.js
+++ frontend/app/scripts/controllers/adminDash/admindash-tutors.js
@@ -8,7 +8,7 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('AdmindashTutorsCtrl', function ($scope, contactAdminFactory, userFactory, $filter) {
+  .controller('AdmindashTutorsCtrl', function ($scope, contactAdminFactory, userFactory, $filter, $uibModal) {
     console.log('come on');
 
     $scope.ugh = new Array();
@@ -21,6 +21,32 @@ angular.module('frontendApp')
       //console.log($scope.ugh[0].applicantInfo.user_)
       console.log($scope.ugh);
     });
+
+
+    $scope.openTutorModal = function (size, tutorID) {
+      console.log('opening tutor modal');
+      console.log(tutorID);
+       contactAdminFactory.storeTutorID(tutorID);
+
+        for (var i=0; i < $scope.ugh.length; i++){
+            if (contactAdminFactory.getTutorID() == $scope.ugh[i].applicantInfo.tutor_id)
+                contactAdminFactory.storeTutorData($scope.ugh[i]);
+        }
+        $scope.testMe = JSON.parse(contactAdminFactory.getTutorData());
+
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'views/adminDash/tutormodal.html',
+        controller: 'TutormodalCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+    };
+
 
     var orderBy = $filter('orderBy');
     $scope.order = function(predicate) {
--- frontend/app/scripts/controllers/adminDash/tutormodal.js
+++ frontend/app/scripts/controllers/adminDash/tutormodal.js
@@ -1,20 +1,3 @@
-//'use strict';
-//
-///**
-// * @ngdoc function
-// * @name frontendApp.controller:TutormodalCtrl
-// * @description
-// * # TutormodalCtrl
-// * Controller of the frontendApp
-// */
-//angular.module('frontendApp')
-//  .controller('TutormodalCtrl', function () {
-//    this.awesomeThings = [
-//      'HTML5 Boilerplate',
-//      'AngularJS',
-//      'Karma'
-//    ];
-//  });
 'use strict';
 
 /**
@@ -27,13 +10,16 @@
 angular.module('frontendApp')
   .controller('TutormodalCtrl', function (contactAdminFactory, $scope) {
 
-    console.log(contactAdminFactory.getAppID());
+    // console.log(contactAdminFactory.getAppID());
+    //
+    // console.log('In addModal');
+    //
+    // console.log(JSON.parse(contactAdminFactory.getModalData()));
 
-    console.log('In addModal');
-
-    console.log(JSON.parse(contactAdminFactory.getModalData()));
-
-    $scope.tits = JSON.parse(contactAdminFactory.getModalData());
+    $scope.tits = JSON.parse(contactAdminFactory.getTutorData());
+    console.log('LOOK AT ME');
+    console.log($scope.tits);
+    console.log($scope.tits.applicantInfo);
 
     $scope.ugh = new Array();
     $scope.real = new Array();
@@ -46,10 +32,12 @@ angular.module('frontendApp')
 //    {"Sun": false, "Mon": false ,"Tues": false ,"Wed": true ,"Thurs": false ,"Fri": false },
 //    {"Sun": false, "Mon": false ,"Tues": false ,"Wed": false ,"Thurs": false ,"Fri": false },
 //    {"Sun": true, "Mon": false ,"Tues": false ,"Wed": false ,"Thurs": false ,"Fri": false }]};
-    var shitData = $scope.tits.calArray;
 
-    $scope.ugh = shitData;
-//    $scope.ugh = shitData.data;
+  var shitData = $scope.tits.calArray;
+
+   $scope.ugh = shitData;
+
+
     var start = 2;
     var end = 3;
     for (var x = 0; x<$scope.ugh.length; x++) {
@@ -57,8 +45,8 @@ angular.module('frontendApp')
       start++;
       end++;
     }
-    console.log('ugh');
-    console.log($scope.ugh);
-    
-  });
 
+    // console.log('ugh');
+    // console.log($scope.ugh);
+
+  });
