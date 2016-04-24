4012042bb4b161226d53d92aaa4d820b7265cda3
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-04-23 23:27:27 -0500

    fixing :poop:

--- backend/db_design/alec.sql
+++ backend/db_design/alec.sql
@@ -3,13 +3,13 @@ CREATE DATABASE alecDB;
 USE alecDB ;
 
 CREATE TABLE Tutors (
-  'tutor_id' INT NOT NULL,
-  'tutor_first_name' VARCHAR(45) NULL,
-  'tutor_last_name' VARCHAR(45) NULL,
-  'tutor_email' VARCHAR(45) NULL,
-  'tutor_phone' VARCHAR(45) NULL,
-  'tutor_gpa' VARCHAR(5) NULL,
-  'tutor_major' VARCHAR(45) NULL,
+  'tutor_id' INT,
+  'tutor_first_name' VARCHAR(45) ,
+  'tutor_last_name' VARCHAR(45) ,
+  'tutor_email' VARCHAR(45) ,
+  'tutor_phone' VARCHAR(45) ,
+  'tutor_gpa' VARCHAR(5) ,
+  'tutor_major' VARCHAR(45) ,
   PRIMARY KEY ('tutor_id'));
 
 
@@ -18,10 +18,10 @@ CREATE TABLE Tutors (
 -- -----------------------------------------------------
 
 CREATE TABLE Timeslots (
-  'timeslot_id' INT NOT NULL AUTO_INCREMENT,
-  'timeslot_time' INT NOT NULL,
-  'Tutors_tutor_id' INT NOT NULL,
-  'timeslot_day' INT NOT NULL,
+  'timeslot_id' INT AUTO_INCREMENT,
+  'timeslot_time' INT,
+  'Tutors_tutor_id' INT,
+  'timeslot_day' INT,
   PRIMARY KEY ('timeslot_id'));
 
 
@@ -30,16 +30,16 @@ CREATE TABLE Timeslots (
 -- -----------------------------------------------------
 --
 -- CREATE TABLE  'Sessions' (
---   'session_id' INT NOT NULL AUTO_INCREMENT,
---   'session_date' DATE NULL,
---   'session_time' VARCHAR(45) NULL,
---   'Tutors_tutor_id' INT NOT NULL,
---   'student_id' VARCHAR(45) NULL,
---   'student_email' VARCHAR(45) NULL,
---   'session_start_time' VARCHAR(45) NULL,
---   'session_end_time' VARCHAR(45) NULL,
---   'student_first_name' VARCHAR(45) NULL,
---   'student_last_name' VARCHAR(45) NULL,
+--   'session_id' INT  AUTO_INCREMENT,
+--   'session_date' DATE ,
+--   'session_time' VARCHAR(45) ,
+--   'Tutors_tutor_id' INT,
+--   'student_id' VARCHAR(45) ,
+--   'student_email' VARCHAR(45) ,
+--   'session_start_time' VARCHAR(45) ,
+--   'session_end_time' VARCHAR(45) ,
+--   'student_first_name' VARCHAR(45) ,
+--   'student_last_name' VARCHAR(45) ,
 --   PRIMARY KEY ('session_id'));
 
 
@@ -48,14 +48,14 @@ CREATE TABLE Timeslots (
 -- -----------------------------------------------------
 
 CREATE TABLE Applicants (
-  'applicant_id' INT NOT NULL,
-  'applicant_first_name' VARCHAR(45) NULL,
-  'applicant_last_name' VARCHAR(45) NULL,
-  'applicant_email' VARCHAR(45) NULL,
-  'applicant_gpa' VARCHAR(5) NULL,
-  'applicant_major' VARCHAR(45) NULL,
-  'application_status' VARCHAR(45) NULL,
-  'applicant_hash' VARCHAR(60) NULL,
+  'applicant_id' INT,
+  'applicant_first_name' VARCHAR(45) ,
+  'applicant_last_name' VARCHAR(45) ,
+  'applicant_email' VARCHAR(45) ,
+  'applicant_gpa' VARCHAR(5) ,
+  'applicant_major' VARCHAR(45) ,
+  'application_status' VARCHAR(45) ,
+  'applicant_hash' VARCHAR(60) ,
   PRIMARY KEY ('applicant_id'));
 
 
@@ -64,10 +64,10 @@ CREATE TABLE Applicants (
 -- -----------------------------------------------------
 
 CREATE TABLE ApplicantTimeslots (
-  'timeslot_id' INT NOT NULL AUTO_INCREMENT,
-  'timeslot_time' INT NOT NULL,
-  'applicant_id' INT NOT NULL,
-  'timeslot_day' INT NOT NULL,
+  'timeslot_id' INT AUTO_INCREMENT,
+  'timeslot_time' INT,
+  'applicant_id' INT,
+  'timeslot_day' INT,
   PRIMARY KEY ('timeslot_id'));
 
 
@@ -76,13 +76,13 @@ CREATE TABLE ApplicantTimeslots (
 -- -----------------------------------------------------
 
 -- CREATE TABLE  'Reviews' (
---   'review_id' INT NOT NULL AUTO_INCREMENT,
---   'score_helpfulness' INT NULL,
---   'score_clarity' INT NULL,
---   'score_friendlynes' INT NULL,
---   'session_id' INT NOT NULL,
---   'score_avg' INT NULL,
---   'review_comments' VARCHAR(500) NULL,
+--   'review_id' INT  AUTO_INCREMENT,
+--   'score_helpfulness' INT ,
+--   'score_clarity' INT ,
+--   'score_friendlynes' INT ,
+--   'session_id' INT,
+--   'score_avg' INT ,
+--   'review_comments' VARCHAR(500) ,
 --   PRIMARY KEY ('review_id'));
 
 
@@ -91,9 +91,9 @@ CREATE TABLE ApplicantTimeslots (
 -- -----------------------------------------------------
 
 CREATE TABLE Users (
-  'user_id' INT NOT NULL,
-  'hash' VARCHAR(60) NULL,
-  'admin' TINYINT(1) NULL DEFAULT 0,
+  'user_id' INT,
+  'hash' VARCHAR(60) ,
+  'admin' TINYINT(1)  DEFAULT 0,
   PRIMARY KEY ('user_id'));
 
 
@@ -101,11 +101,11 @@ CREATE TABLE Users (
 -- Table 'Admins'
 -- -----------------------------------------------------
 -- CREATE TABLE  'Admins' (
---   'admin_id' INT NOT NULL,
---   'admin_first_name' VARCHAR(45) NULL,
---   'admin_last_name' VARCHAR(45) NULL,
---   'admin_email' VARCHAR(45) NULL,
---   'admin_phone' VARCHAR(45) NULL,
+--   'admin_id' INT,
+--   'admin_first_name' VARCHAR(45) ,
+--   'admin_last_name' VARCHAR(45) ,
+--   'admin_email' VARCHAR(45) ,
+--   'admin_phone' VARCHAR(45) ,
 --   PRIMARY KEY ('admin_id'));
 
 
@@ -114,11 +114,11 @@ CREATE TABLE Users (
 -- -----------------------------------------------------
 
 CREATE TABLE TutorClasses (
-  'tutorclass_id' INT NOT NULL AUTO_INCREMENT,
-  'class_type' VARCHAR(4) NULL,
-  'class_num' VARCHAR(4) NULL,
-  'class_grade' VARCHAR(3) NULL,
-  'tutor_id' INT NOT NULL,
+  'tutorclass_id' INT AUTO_INCREMENT,
+  'class_type' VARCHAR(4) ,
+  'class_num' VARCHAR(4) ,
+  'class_grade' VARCHAR(3) ,
+  'tutor_id' INT,
   PRIMARY KEY ('tutorclass_id'));
 
 
@@ -127,11 +127,11 @@ CREATE TABLE TutorClasses (
 -- -----------------------------------------------------
 
 CREATE TABLE ApplicantClasses (
-  'idApplicantClasses' INT NOT NULL AUTO_INCREMENT,
-  'class_name' VARCHAR(45) NULL,
-  'class_gpa' VARCHAR(2) NULL,
-  'class_number' VARCHAR(4) NULL,
-  'applicant_id' INT NOT NULL,
+  'idApplicantClasses' INT AUTO_INCREMENT,
+  'class_name' VARCHAR(45) ,
+  'class_gpa' VARCHAR(2) ,
+  'class_number' VARCHAR(4) ,
+  'applicant_id' INT,
   PRIMARY KEY ('idApplicantClasses'));
 
 
@@ -140,11 +140,11 @@ CREATE TABLE ApplicantClasses (
 -- -----------------------------------------------------
 
 CREATE TABLE TutorRequests (
-  'tr_id' INT NOT NULL AUTO_INCREMENT,
-  'tr_tutor_id' INT NULL,
-  'tr_classtype' VARCHAR(4) NULL,
-  'tr_classnum' VARCHAR(4) NULL,
-  'tr_request_type' VARCHAR(4) NULL,
+  'tr_id' INT AUTO_INCREMENT,
+  'tr_tutor_id' INT ,
+  'tr_classtype' VARCHAR(4) ,
+  'tr_classnum' VARCHAR(4) ,
+  'tr_request_type' VARCHAR(4) ,
   PRIMARY KEY ('tr_id'));
 
 
