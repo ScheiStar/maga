537d3c1c2aa76a21d3fbba9102a0b50750df1944
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-04-23 23:07:37 -0500

    fixing :poop:

--- backend/db_design/alec.sql
+++ backend/db_design/alec.sql
@@ -14,171 +14,171 @@
 -- -----------------------------------------------------
 -- Schema alecDB
 -- -----------------------------------------------------
-DROP DATABASE IF EXISTS`alecDB`;
-CREATE DATABASE `alecDB`;
-USE `alecDB` ;
+DROP DATABASE IF EXISTS'alecDB';
+CREATE DATABASE 'alecDB';
+USE 'alecDB' ;
 
 -- -----------------------------------------------------
--- Table `alecDB`.`Tutors`
+-- Table 'alecDB'.'Tutors'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Tutors` ;
+DROP TABLE IF EXISTS 'alecDB'.'Tutors' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`Tutors` (
-  `tutor_id` INT NOT NULL,
-  `tutor_first_name` VARCHAR(45) NULL,
-  `tutor_last_name` VARCHAR(45) NULL,
-  `tutor_email` VARCHAR(45) NULL,
-  `tutor_phone` VARCHAR(45) NULL,
-  `tutor_gpa` VARCHAR(5) NULL,
-  `tutor_major` VARCHAR(45) NULL,
-  PRIMARY KEY (`tutor_id`));
-
-
--- -----------------------------------------------------
--- Table `alecDB`.`Timeslots`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Timeslots` ;
-
-CREATE TABLE IF NOT EXISTS `alecDB`.`Timeslots` (
-  `timeslot_id` INT NOT NULL AUTO_INCREMENT,
-  `timeslot_time` INT NOT NULL,
-  `Tutors_tutor_id` INT NOT NULL,
-  `timeslot_day` INT NOT NULL,
-  PRIMARY KEY (`timeslot_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Tutors' (
+  'tutor_id' INT NOT NULL,
+  'tutor_first_name' VARCHAR(45) NULL,
+  'tutor_last_name' VARCHAR(45) NULL,
+  'tutor_email' VARCHAR(45) NULL,
+  'tutor_phone' VARCHAR(45) NULL,
+  'tutor_gpa' VARCHAR(5) NULL,
+  'tutor_major' VARCHAR(45) NULL,
+  PRIMARY KEY ('tutor_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`Sessions`
+-- Table 'alecDB'.'Timeslots'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Sessions` ;
+DROP TABLE IF EXISTS 'alecDB'.'Timeslots' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`Sessions` (
-  `session_id` INT NOT NULL AUTO_INCREMENT,
-  `session_date` DATE NULL,
-  `session_time` VARCHAR(45) NULL,
-  `Tutors_tutor_id` INT NOT NULL,
-  `student_id` VARCHAR(45) NULL,
-  `student_email` VARCHAR(45) NULL,
-  `session_start_time` VARCHAR(45) NULL,
-  `session_end_time` VARCHAR(45) NULL,
-  `student_first_name` VARCHAR(45) NULL,
-  `student_last_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`session_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Timeslots' (
+  'timeslot_id' INT NOT NULL AUTO_INCREMENT,
+  'timeslot_time' INT NOT NULL,
+  'Tutors_tutor_id' INT NOT NULL,
+  'timeslot_day' INT NOT NULL,
+  PRIMARY KEY ('timeslot_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`Applicants`
+-- Table 'alecDB'.'Sessions'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Applicants` ;
+DROP TABLE IF EXISTS 'alecDB'.'Sessions' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`Applicants` (
-  `applicant_id` INT NOT NULL,
-  `applicant_first_name` VARCHAR(45) NULL,
-  `applicant_last_name` VARCHAR(45) NULL,
-  `applicant_email` VARCHAR(45) NULL,
-  `applicant_gpa` VARCHAR(5) NULL,
-  `applicant_major` VARCHAR(45) NULL,
-  `application_status` VARCHAR(45) NULL,
-  `applicant_hash` VARCHAR(60) NULL,
-  PRIMARY KEY (`applicant_id`));
-
-
--- -----------------------------------------------------
--- Table `alecDB`.`ApplicantTimeslots`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`ApplicantTimeslots` ;
-
-CREATE TABLE IF NOT EXISTS `alecDB`.`ApplicantTimeslots` (
-  `timeslot_id` INT NOT NULL AUTO_INCREMENT,
-  `timeslot_time` INT NOT NULL,
-  `Applicant_applicant_id` INT NOT NULL,
-  `timeslot_day` INT NOT NULL,
-  PRIMARY KEY (`timeslot_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Sessions' (
+  'session_id' INT NOT NULL AUTO_INCREMENT,
+  'session_date' DATE NULL,
+  'session_time' VARCHAR(45) NULL,
+  'Tutors_tutor_id' INT NOT NULL,
+  'student_id' VARCHAR(45) NULL,
+  'student_email' VARCHAR(45) NULL,
+  'session_start_time' VARCHAR(45) NULL,
+  'session_end_time' VARCHAR(45) NULL,
+  'student_first_name' VARCHAR(45) NULL,
+  'student_last_name' VARCHAR(45) NULL,
+  PRIMARY KEY ('session_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`Reviews`
+-- Table 'alecDB'.'Applicants'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Reviews` ;
+DROP TABLE IF EXISTS 'alecDB'.'Applicants' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`Reviews` (
-  `review_id` INT NOT NULL AUTO_INCREMENT,
-  `score_helpfulness` INT NULL,
-  `score_clarity` INT NULL,
-  `score_friendlynes` INT NULL,
-  `Sessions_session_id` INT NOT NULL,
-  `score_avg` INT NULL,
-  `review_comments` VARCHAR(500) NULL,
-  PRIMARY KEY (`review_id`));
-
-
--- -----------------------------------------------------
--- Table `alecDB`.`Users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Users` ;
-
-CREATE TABLE IF NOT EXISTS `alecDB`.`Users` (
-  `user_id` INT NOT NULL,
-  `hash` VARCHAR(60) NULL,
-  `admin` TINYINT(1) NULL DEFAULT 0,
-  PRIMARY KEY (`user_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Applicants' (
+  'applicant_id' INT NOT NULL,
+  'applicant_first_name' VARCHAR(45) NULL,
+  'applicant_last_name' VARCHAR(45) NULL,
+  'applicant_email' VARCHAR(45) NULL,
+  'applicant_gpa' VARCHAR(5) NULL,
+  'applicant_major' VARCHAR(45) NULL,
+  'application_status' VARCHAR(45) NULL,
+  'applicant_hash' VARCHAR(60) NULL,
+  PRIMARY KEY ('applicant_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`Admins`
+-- Table 'alecDB'.'ApplicantTimeslots'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`Admins` ;
+DROP TABLE IF EXISTS 'alecDB'.'ApplicantTimeslots' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`Admins` (
-  `admin_id` INT NOT NULL,
-  `admin_first_name` VARCHAR(45) NULL,
-  `admin_last_name` VARCHAR(45) NULL,
-  `admin_email` VARCHAR(45) NULL,
-  `admin_phone` VARCHAR(45) NULL,
-  PRIMARY KEY (`admin_id`));
-
-
--- -----------------------------------------------------
--- Table `alecDB`.`TutorClasses`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`TutorClasses` ;
-
-CREATE TABLE IF NOT EXISTS `alecDB`.`TutorClasses` (
-  `tutorclass_id` INT NOT NULL AUTO_INCREMENT,
-  `class_type` VARCHAR(4) NULL,
-  `class_num` VARCHAR(4) NULL,
-  `class_grade` VARCHAR(3) NULL,
-  `Tutors_tutor_id` INT NOT NULL,
-  PRIMARY KEY (`tutorclass_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'ApplicantTimeslots' (
+  'timeslot_id' INT NOT NULL AUTO_INCREMENT,
+  'timeslot_time' INT NOT NULL,
+  'Applicant_applicant_id' INT NOT NULL,
+  'timeslot_day' INT NOT NULL,
+  PRIMARY KEY ('timeslot_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`ApplicantClasses`
+-- Table 'alecDB'.'Reviews'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`ApplicantClasses` ;
+DROP TABLE IF EXISTS 'alecDB'.'Reviews' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`ApplicantClasses` (
-  `idApplicantClasses` INT NOT NULL AUTO_INCREMENT,
-  `class_name` VARCHAR(45) NULL,
-  `class_gpa` VARCHAR(2) NULL,
-  `class_number` VARCHAR(4) NULL,
-  `Applicants_applicant_id` INT NOT NULL,
-  PRIMARY KEY (`idApplicantClasses`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Reviews' (
+  'review_id' INT NOT NULL AUTO_INCREMENT,
+  'score_helpfulness' INT NULL,
+  'score_clarity' INT NULL,
+  'score_friendlynes' INT NULL,
+  'Sessions_session_id' INT NOT NULL,
+  'score_avg' INT NULL,
+  'review_comments' VARCHAR(500) NULL,
+  PRIMARY KEY ('review_id'));
 
 
 -- -----------------------------------------------------
--- Table `alecDB`.`TutorRequests`
+-- Table 'alecDB'.'Users'
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `alecDB`.`TutorRequests` ;
+DROP TABLE IF EXISTS 'alecDB'.'Users' ;
 
-CREATE TABLE IF NOT EXISTS `alecDB`.`TutorRequests` (
-  `tr_id` INT NOT NULL AUTO_INCREMENT,
-  `tr_tutor_id` INT NULL,
-  `tr_classtype` VARCHAR(4) NULL,
-  `tr_classnum` VARCHAR(4) NULL,
-  `Tutors_tutor_id` INT NOT NULL,
-  `tr_request_type` VARCHAR(4) NULL,
-  PRIMARY KEY (`tr_id`));
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Users' (
+  'user_id' INT NOT NULL,
+  'hash' VARCHAR(60) NULL,
+  'admin' TINYINT(1) NULL DEFAULT 0,
+  PRIMARY KEY ('user_id'));
+
+
+-- -----------------------------------------------------
+-- Table 'alecDB'.'Admins'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'alecDB'.'Admins' ;
+
+CREATE TABLE IF NOT EXISTS 'alecDB'.'Admins' (
+  'admin_id' INT NOT NULL,
+  'admin_first_name' VARCHAR(45) NULL,
+  'admin_last_name' VARCHAR(45) NULL,
+  'admin_email' VARCHAR(45) NULL,
+  'admin_phone' VARCHAR(45) NULL,
+  PRIMARY KEY ('admin_id'));
+
+
+-- -----------------------------------------------------
+-- Table 'alecDB'.'TutorClasses'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'alecDB'.'TutorClasses' ;
+
+CREATE TABLE IF NOT EXISTS 'alecDB'.'TutorClasses' (
+  'tutorclass_id' INT NOT NULL AUTO_INCREMENT,
+  'class_type' VARCHAR(4) NULL,
+  'class_num' VARCHAR(4) NULL,
+  'class_grade' VARCHAR(3) NULL,
+  'Tutors_tutor_id' INT NOT NULL,
+  PRIMARY KEY ('tutorclass_id'));
+
+
+-- -----------------------------------------------------
+-- Table 'alecDB'.'ApplicantClasses'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'alecDB'.'ApplicantClasses' ;
+
+CREATE TABLE IF NOT EXISTS 'alecDB'.'ApplicantClasses' (
+  'idApplicantClasses' INT NOT NULL AUTO_INCREMENT,
+  'class_name' VARCHAR(45) NULL,
+  'class_gpa' VARCHAR(2) NULL,
+  'class_number' VARCHAR(4) NULL,
+  'Applicants_applicant_id' INT NOT NULL,
+  PRIMARY KEY ('idApplicantClasses'));
+
+
+-- -----------------------------------------------------
+-- Table 'alecDB'.'TutorRequests'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'alecDB'.'TutorRequests' ;
+
+CREATE TABLE IF NOT EXISTS 'alecDB'.'TutorRequests' (
+  'tr_id' INT NOT NULL AUTO_INCREMENT,
+  'tr_tutor_id' INT NULL,
+  'tr_classtype' VARCHAR(4) NULL,
+  'tr_classnum' VARCHAR(4) NULL,
+  'Tutors_tutor_id' INT NOT NULL,
+  'tr_request_type' VARCHAR(4) NULL,
+  PRIMARY KEY ('tr_id'));
 
 
 -- SET SQL_MODE=@OLD_SQL_MODE;
